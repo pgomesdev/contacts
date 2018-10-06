5793fac845f82a6cdd3a03ccd55dd74101c98444
Pedro Gomes <gomes.pedro15@gmail.com>
2018-10-06 10:08:31 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
+  state = {
+    contacts: [],
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then(contacts => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id;
+      })
+    }));
+
+    ContactsAPI.remove(contact);
+  }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState(prevState => ({
+          contacts: [...prevState.contacts, contact]
+        }))
+      })
+  }
+
   render() {
-    
+    return (
+      <div>
+        <Route exact path='/' render={() => (
+          <ListContacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact);
+              history.push('/');
+            }}
+          />
+        )} />
+      </div>
+    );
   }
 }
 
